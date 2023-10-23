f2e9d78e8f08691c4f2cfb957160f95b4fa9feb2
haegoal <ppd5479@gmail.com>
2023-10-23 19:11:41 +0900

    쿠폰함까지 백업

--- src/main/java/com/example/reservation/controller/HomeController.java
+++ src/main/java/com/example/reservation/controller/HomeController.java
@@ -1,11 +1,21 @@
 package com.example.reservation.controller;
 
+import com.example.reservation.dto.NoticeDTO;
+import com.example.reservation.dto.RoomDTO;
+import com.example.reservation.service.MemberService;
+import com.example.reservation.service.NoticeService;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final NoticeService noticeService;
 
     @GetMapping("/")
     public String index() {
@@ -13,7 +23,7 @@ public class HomeController {
     }
 
     @GetMapping("/room")
-    public String roomIndex() {
+    public String roomIndex(Model model) {
         return "roomList";
     }
 
