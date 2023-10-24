4c3e90eedefbe7624dd02ce5b2b3ebf63c21d00e
jscheo <981625@naver.com>
2023-10-24 21:16:57 +0900

    쿠폰상태 int로 변환 / 쿠폰리스트 사용가능한 쿠폰만 출력하도록 변경처리

--- src/main/java/com/example/reservation/repository/CouponRepository.java
+++ src/main/java/com/example/reservation/repository/CouponRepository.java
@@ -3,9 +3,15 @@ package com.example.reservation.repository;
 import com.example.reservation.entity.CouponEntity;
 import com.example.reservation.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CouponRepository extends JpaRepository<CouponEntity, Long> {
     List<CouponEntity> findByMemberEntity(MemberEntity memberEntity);
+
+    @Query("SELECT c FROM CouponEntity c WHERE c.memberEntity = :member AND c.couponStatus = 0")
+    List<CouponEntity> findZeroValueCoupons(@Param("member") MemberEntity memberEntity);
+
 }
