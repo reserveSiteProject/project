7ee575e63e585b5afb57b6c44ed0ad2d78143953
haegoal <ppd5479@gmail.com>
2023-10-25 13:40:46 +0900

    파일 업로드 경로 변경 / DTO값 추가

--- src/main/java/com/example/reservation/dto/ReserveDTO.java
+++ src/main/java/com/example/reservation/dto/ReserveDTO.java
@@ -2,10 +2,15 @@ package com.example.reservation.dto;
 
 
 import com.example.reservation.entity.ReserveEntity;
+import com.example.reservation.entity.RoomFileEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Builder
@@ -22,14 +27,28 @@ public class ReserveDTO {
     private String checkOutDate;
     private int persons;
 
+    private int fileAttached;
+    private String roomName;
+    private List<MultipartFile> RoomFileName;
+    private List<String> originalFileName = new ArrayList<>();
+    private List<String> storedFileName = new ArrayList<>();
+
+
     public static ReserveDTO toDTO(ReserveEntity reserveEntity) {
         ReserveDTO reserveDTO = new ReserveDTO();
+        reserveDTO.setRoomName(reserveEntity.getRoomEntity().getRoomName());
+        for (RoomFileEntity roomFileEntity: reserveEntity.getRoomEntity().getRoomFileEntityList()) {
+            reserveDTO.getOriginalFileName().add(roomFileEntity.getOriginalFileName());
+            reserveDTO.getStoredFileName().add(roomFileEntity.getStoredFileName());
+        }
         reserveDTO.setId(reserveEntity.getId());
+        reserveDTO.setFileAttached(reserveEntity.getRoomEntity().getFileAttached());
         reserveDTO.setMemberId(reserveEntity.getMemberEntity().getId());
         reserveDTO.setRoomId(reserveEntity.getRoomEntity().getId());
         reserveDTO.setCheckInDate(reserveEntity.getCheckInDate());
         reserveDTO.setCheckOutDate(reserveEntity.getCheckOutDate());
         reserveDTO.setPersons(reserveDTO.getPersons());
+        System.out.println("reserveDTO = " + reserveDTO);
         return reserveDTO;
     }
 
