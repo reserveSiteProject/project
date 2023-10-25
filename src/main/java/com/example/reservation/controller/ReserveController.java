6e23e0e1e7a6fd4b24da5e0424bca57405589c2c
haegoal <ppd5479@gmail.com>
2023-10-25 20:51:53 +0900

    날짜선택시 객실 파일, 룸정보 가지고오기, 객실 인원수 구하기완료

--- src/main/java/com/example/reservation/controller/ReserveController.java
+++ src/main/java/com/example/reservation/controller/ReserveController.java
@@ -1,17 +1,26 @@
 package com.example.reservation.controller;
 
+import com.example.reservation.dto.RoomDTO;
 import com.example.reservation.service.ReserveService;
+import com.example.reservation.service.RoomService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/reserve")
 public class ReserveController {
     private final ReserveService reserveService;
+    private final RoomService roomService;
 
     @GetMapping
     public String book() {
@@ -19,8 +28,10 @@ public class ReserveController {
     }
 
     @GetMapping("/list")
-    public String list(@RequestParam("checkInDate") String checkInDate, @RequestParam("checkOutDate") String checkOutDate) {
-        System.out.println("checkInDate = " + checkInDate + ", checkOutDate = " + checkOutDate);
-        return "";
+//    public String list(@RequestParam("checkInDate") String checkInDate, @RequestParam("checkOutDate") String checkOutDate) {
+    public ResponseEntity list(Model model) {
+        List<RoomDTO> roomDTOList = roomService.findAll();
+        System.out.println("roomDTOList = " + roomDTOList);
+        return new ResponseEntity<>(roomDTOList, HttpStatus.OK);
     }
 }
