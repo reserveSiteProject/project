27b01e49e2e537f6d1f8bfd2396e7b0d321199f0
THproger13 <thphysics@naver.com>
2023-10-25 23:12:15 +0900

    reviewSave 기능 디버깅

--- src/main/java/com/example/reservation/entity/ReviewFileEntity.java
+++ src/main/java/com/example/reservation/entity/ReviewFileEntity.java
@@ -22,7 +22,19 @@ public class ReviewFileEntity {
     @Column(length = 200)
     private String storedFileName;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+//    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+
+    /*
+    왜 첨부파일 저장 처리가 정상적으로 되었는지:
+    cascade = CascadeType.ALL 설정이 있을 때 ReviewFileEntity를 저장하는 동안 ReviewEntity에도 변경이 전파되려고 합니다.
+    이때, ReviewEntity가 이미 데이터베이스에 저장되어 있어 변경이 필요 없음에도 불구하고 변경을 시도하면 문제가 발생할 수 있습니다.
+    주석이 해제된 설정에서는 ReviewFileEntity만 저장하면 되기 때문에 별도의 문제 없이 정상적으로 처리됩니다.
+    결론적으로, ReviewEntity와 ReviewFileEntity 사이의 관계 설정에서 cascade 옵션을 어떻게 설정하느냐에 따라 영향을 받는 것입니다.
+     만약 ReviewEntity를 저장하면서 연관된 ReviewFileEntity도 함께 저장하려면 cascade 옵션을 사용하면 됩니다.
+     하지만 이미 저장된 ReviewEntity에 연관된 ReviewFileEntity만 따로 저장하려면 cascade 옵션을 사용하지 않는 것이 좋습니다.
+     */
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "review_id")
     private ReviewEntity reviewEntity;
     public static ReviewFileEntity toSaveReviewFile(ReviewEntity savedEntity, String originalFilename, String storedFileName) {
--- src/main/java/com/example/reservation/repository/ReserveRepository.java
+++ src/main/java/com/example/reservation/repository/ReserveRepository.java
@@ -5,6 +5,7 @@ import com.example.reservation.entity.ReserveEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReserveRepository extends JpaRepository<ReserveEntity, Long> {
     List<ReserveEntity> findByMemberEntity(MemberEntity memberEntity);
