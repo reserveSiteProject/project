e686d3bca0f8a286054d128da35ea1d64292a179
haegoal <ppd5479@gmail.com>
2023-11-15 10:31:00 +0900

    페이지 수정

--- src/main/java/com/example/reservation/config/WebConfig.java
+++ src/main/java/com/example/reservation/config/WebConfig.java
@@ -13,10 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginCheckInterceptor()) // 인터셉터로 등록할 클래스
                 .order(1) // 해당 인터셉터의 우선순위
-                .addPathPatterns("/myPages/*", "/admin/*", "/book/*", "/board/review/save", "/board/notice/save") // 인터셉터로 체크할 주소(모든주소)
+                .addPathPatterns("/admin/*", "/book/*", "/board/review/save", "/board/notice/save") // 인터셉터로 체크할 주소(모든주소)
                 .excludePathPatterns("/", "/member/save", "/member/login",
                         "/js/**", "/css/**", "/member/image/**", "/image/**",
-                        "/*.ico", "/favicon/**", "ws://localhost:8080/ws/chat"); // 인터셉터 검증을 하지 않을 주소
+                        "/*.ico", "/favicon/**", "ws://localhost:8080/ws/chat", "/board", "/myPages/*"); // 인터셉터 검증을 하지 않을 주소
     }
 
     private String resourcePath = "/upload/**"; // html 에서 접근할 경로
